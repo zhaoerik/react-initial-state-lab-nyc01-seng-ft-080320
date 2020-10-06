b0e01dc6651cfac5210cca956281da7a1240e3de
Erik Zhao <erikzhao98@gmail.com>
2020-10-06 02:28:19 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,26 @@
 // your Bomb code here!
+// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        if (this.state.secondsLeft = 0) {
+        return (
+            <h2> Boom! </h2>
+        )
+    } else {
+        return (
+            <h2>{this.state.secondsLeft} seconds left before I go boom!</h2>
+        )    
+        }
+    }
+}
+
+export default Bomb;
