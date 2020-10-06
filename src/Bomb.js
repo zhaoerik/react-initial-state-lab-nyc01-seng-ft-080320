75b0da45a009e424985519f84932bb529e4c012a
Erik Zhao <erikzhao98@gmail.com>
2020-10-06 02:35:39 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -10,17 +10,17 @@ class Bomb extends React.Component {
         }
     }
 
-    render() {
-        if (this.state.secondsLeft = 0) {
-        return (
-            <h2> Boom! </h2>
-        )
-    } else {
-        return (
-            <h2>{this.state.secondsLeft} seconds left before I go boom!</h2>
-        )    
-        }
-    }
-}
+//     render() {
+//         if (this.state.secondsLeft = 0) {
+//         return (
+//             <h2> Boom! </h2>
+//         )
+//     } else {
+//         return (
+//             <h2>{this.state.secondsLeft} seconds left before I go boom!</h2>
+//         )    
+//         }
+//     }
+// }
 
 export default Bomb;
